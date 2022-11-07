e9ccccd38ea914bbcef5434f8f0dbaa3795a9957
marios_kontzos <mkontzos@allweb.gr>
2022-11-07 22:20:35 +0200

    preparing controllers

---
+++ CustomerOrderProduct/CustomerOrderProduct/Controllers/CustomerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerOrderProduct.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CustomerController : ControllerBase
+	{
+		public ActionResult Index()
+		{
+			return null;
+		}
+	}
+}
---
+++ CustomerOrderProduct/CustomerOrderProduct/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerOrderProduct.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class OrderController : ControllerBase
+	{
+		public ActionResult Index()
+		{
+			return null;
+		}
+	}
+}
